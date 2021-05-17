8192ae891f742b3113718d9494d1f735b794b78b
natsuokawai <kawai.natsuo@gmail.com>
2021-05-17 09:24:33 +0900

    Add multipart/form-data

---
+++ multipartform/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"os"
+)
+
+func main() {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Natsuo Kawai")
+	filename := "photo.jpg"
+	part := make(textproto.MIMEHeader)
+	part.Set("Content-Type", "image/jpeg")
+	part.Set("Content-Disposition", `form-data; name="thumbnail"; filename="`+filename+`"`)
+	fileWriter, err := writer.CreatePart(part)
+	if err != nil {
+		panic(err)
+	}
+	readFile, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:" + resp.Status)
+}
